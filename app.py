d92ba9ac46d90343f7f1fe5c00acacf7dda400cf
Haider <shkhaider2015@gmail.com>
2020-03-15 02:00:40 +0500

    login

--- app.py
+++ app.py
@@ -41,9 +41,15 @@ def register():
     return render_template('register.html', title='Register', form=form)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@blog.com" and form.password.data == "password":
+            flash(f"You have been logged in", 'success')
+            return redirect(url_for('home'))
+        else:
+            flash("Check username and password", 'danger')
     return render_template('login.html', title='login', form=form)
 
 
