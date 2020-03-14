7dc46118bd92ad82d7c28e6cb28ec1053bbfadf3
Haider <shkhaider2015@gmail.com>
2020-03-14 18:20:17 +0500

    register

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = ''
+app.config['SECRET_KEY'] = 'f6cd85d264d6ceb0ad9a11d7be35a930'
 
 posts = [
     {
@@ -30,3 +31,14 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='login', form=form)
