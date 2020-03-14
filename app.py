6e5aa18d29712e4b2d8002e1158712de5f62faf0
Haider <shkhaider2015@gmail.com>
2020-03-14 18:45:44 +0500

    login error

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
@@ -32,9 +32,12 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data} !', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
 
@@ -42,3 +45,8 @@ def register():
 def login():
     form = LoginForm()
     return render_template('login.html', title='login', form=form)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
