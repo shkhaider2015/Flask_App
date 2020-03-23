a6c1c21e10bf362c027d0f88c586911bccaf1fc7
Haider <shkhaider2015@gmail.com>
2020-03-24 00:33:42 +0500

    ff

--- myApp/__init__.py
+++ myApp/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 
 
@@ -9,5 +10,6 @@ app.config['SECRET_KEY'] = 'f6cd85d264d6ceb0ad9a11d7be35a930'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from myApp import routes
--- myApp/routes.py
+++ myApp/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, url_for, flash, redirect
-from myApp import app
+from myApp import app, db, bcrypt
 from myApp.forms import RegistrationForm, LoginForm
 from myApp.models import User, Post
 
@@ -35,8 +35,12 @@ def about():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data} !', 'success')
-        return redirect(url_for('home'))
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! and you are now able to login', 'success')
+        return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
 
