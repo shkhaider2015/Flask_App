b0c3e8608331e1ba3b565edad2172d7cbf83dce2
Haider <shkhaider2015@gmail.com>
2020-03-31 03:08:30 +0500

    complete reset password

--- myApp/routes.py
+++ myApp/routes.py
@@ -185,8 +185,8 @@ def reset_request():
     return render_template('reset_request.html', title='Reset Password', form=form)
 
 
-@app.route("/reset_password/<string:token>", methods=['GET', 'POST'])
-def reset_token():
+@app.route("/reset_password/<token>", methods=['GET', 'POST'])
+def reset_token(token):
     if current_user.is_authenticated:
         return redirect(url_for('home'))
     user = User.verify_reset_token(token)
