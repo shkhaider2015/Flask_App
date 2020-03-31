14a6c9f747450eaf8187b839c6e119d63f2dc3e8
Haider <shkhaider2015@gmail.com>
2020-04-01 02:41:24 +0500

    complete

--- myApp/__init__.py
+++ myApp/__init__.py
@@ -36,8 +36,11 @@ def create_app(config_class=Config):
     from myApp.users.routes import users
     from myApp.posts.routes import posts
     from myApp.main.routes import main
+    from myApp.errors.handlers import errors
+
     app.register_blueprint(users)
     app.register_blueprint(posts)
     app.register_blueprint(main)
+    app.register_blueprint(errors)
 
     return app
